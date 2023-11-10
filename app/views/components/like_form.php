d1aa00790261cbe974ca31f4f78ef696818eb920
220101ft <220101ft@yse-c.net>
2023-11-10 14:35:19 +0900

    update

--- app/views/components/like_form.php
+++ app/views/components/like_form.php
@@ -6,7 +6,7 @@
 
         <?php if (in_array($tweet['id'], $user_likes)) : ?>
             <button class="btn btn-sm"><img src="../images/svg/heart_active.svg"></button>
-            <span class="like-count"><?= @$like_counts[$tweet['id']] ?></span>
+            <span class="like-count"><?= @$like_counts[$tweet['id']].'1' ?></span>
         <?php else : ?>
             <button class="btn btn-sm"><img src="../images/svg/heart.svg"></button>
             <span class="like-count"><?= @$like_counts[$tweet['id']] ?></span>
